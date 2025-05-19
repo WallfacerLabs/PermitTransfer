49af3264f03f64d35eb4fd15a9b211a1806ea87d
pawelpolak2 <pawel.ulic@gmail.com>
2025-05-19 15:54:14 +0200

    use solidity 0.8.30

--- test/testUtils/MockERC20Permit.t.sol
+++ test/testUtils/MockERC20Permit.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.30;
 
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
